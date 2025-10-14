cbae03595c5fd098abf0026f7b8deac662577538
Arjun059 <arjunchitara35081@gmail.com>
2025-10-14 09:01:35 +0530

    add ping req

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -1,7 +1,12 @@
 import {Outlet} from 'react-router-dom'
 import Header from '#/layouts/header'
+import {useEffect} from 'react'
+import {fetcher} from '../utils/fetcher'
 
 export function RootLayout() {
+  useEffect(() => {
+    fetcher("/ping", {method: "GET"})
+  }, [])
   return (
     <div className="relative flex min-h-screen flex-col">
       <Header />
